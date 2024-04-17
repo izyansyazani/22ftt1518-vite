a34f476ac11fbb0e9170fabcb76fb76058f84478
22FTT1518 <22ftt1518@student.pb.edu.bn>
2024-04-17 22:01:38 +0800

    add about me and profile pic

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,44 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+<script setup lang="ts"></script>
+<template
+  class="p-6 sm:p-12 md:p-24 flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-pink-300 to-purple-400 rounded"
+>
+  <div class="flex justify-center items-center mb-4">
+    <img
+      src="./assets/izyansyazani.jpg"
+      class="w-24 h-24 rounded-full mb-4 shadow-lg border-4 border-white object-cover"
+      alt="Profile Picture"
+    />
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="text-center text-white">
+    <h1 class="text-4xl font-bold mb-4">Nur Izyan Syazani</h1>
+    <p class="text-lg text-gray-100">Student ID: 22FTT1518</p>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8">
+      <div>
+        <h2 class="text-2xl font-semibold mb-4">Currently studying</h2>
+        <ul class="text-lg text-gray-100">
+          <li>WADT</li>
+          <li>IPI</li>
+          <li>MWE</li>
+          <li>PMGT</li>
+          <li>AWEP</li>
+        </ul>
+      </div>
+      <div>
+        <h2 class="text-2xl font-semibold mb-4">Hobbies</h2>
+        <ul class="text-lg text-gray-100">
+          <li>Reading Books</li>
+          <li>Listening to musics</li>
+          <li>Sleeping</li>
+        </ul>
+      </div>
+    </div>
+    <h2 class="text-2xl font-semibold mt-8">Contact</h2>
+    <ul class="text-lg text-gray-100">
+      <li>Email: 22ftt1518@student.pb.edu.bn</li>
+      <li>Phone: 123-456-7890</li>
+      <li>Website: www.example.com</li>
+    </ul>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style></style>
